2837028eaf6cac5ce34344f2879f31abab78b978
Julio Olivera <julio.olvr@gmail.com>
2016-05-03 20:50:24 -0300

    Steam Id input

--- src/app/components/games-list/games-list.ts
+++ src/app/components/games-list/games-list.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from 'angular2/core';
+import {Component, OnInit, Input} from 'angular2/core';
 
 import {Api} from '../../services/api/api';
 
@@ -12,18 +12,23 @@ const STEAM_ID = '76561197995010243';
   directives: [],
   pipes: []
 })
-export class GamesList implements OnInit {
+export class GamesList {
   public games;
   public errorMessage: string;
 
   constructor(private _api: Api) { }
 
-  ngOnInit() {
-    this.getGames();
+  @Input()
+  set steamId(value: string) { // TODO: Is this to react to changes?
+    this.getGames(value);
   }
 
-  getGames() {
-    this._api.getGames(STEAM_ID).subscribe(
+  getGames(steamId) {
+    if (!steamId) {
+      return;
+    }
+
+    this._api.getGames(steamId).subscribe(
       games => this.games = games,
       error => this.errorMessage = error);
   }
